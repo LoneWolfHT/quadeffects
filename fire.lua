12476eec779336d8778f99da92ec1a142c36ea8e
LoneWolfHT <lonewolf04361@gmail.com>
2019-07-15 12:22:31 -0700

    Improve fire effect

--- fire.lua
+++ fire.lua
@@ -1,7 +1,23 @@
-local fire_mod_enabled = minetest.get_modpath("fire")
+local fire_mod_found = false
+local fire_sound
+local fire_sound_gain = 1.0
+local fire_extinguish_sound
+
+if minetest.get_modpath("fire") then
+	fire_mod_found = true
+	fire_sound = "fire_fire"
+	fire_extinguish_sound = "fire_extinguish_flame"
+elseif minetest.get_modpath("nc_fire") then
+	fire_sound_gain = 4
+	qeffects.fire.dps_multiplier = 1.5
+	fire_mod_found = true
+	fire_sound = "nc_fire_flamy"
+	fire_extinguish_sound = "nc_fire_snuff"
+end
 
 function qeffects.fire.apply(name, dps, time)
 	local player = minetest.get_player_by_name(name)
+	local pos = vector.new(0, 0.5, 0)
 
 	if not player then
 		minetest.log("warning", "[QEffects:Fire] Player "..name.." not found. Aborting apply()...")
@@ -19,15 +35,34 @@ function qeffects.fire.apply(name, dps, time)
 		scale     = {x = -100, y = -75},
 	})
 
+	local particles = minetest.add_particlespawner({
+		time = 0,
+		amount = dps * 30,
+		minpos = vector.subtract(pos, 0.5),
+		maxpos = vector.add(pos, 0.5),
+		minvel = {x = -1, y = 3, z = -1},
+		maxvel = {x = 1, y = 4,  z = 1},
+		minacc = {x = 0, y = 3, z = 0},
+		maxacc = {x = 0, y = 5, z = 0},
+		minexptime = 0.2,
+		maxexptime = 0.3,
+		minsize = 4,
+		maxsize = 5,
+		texture = "quadeffects_fire_particle.png",
+		collisiondetection = true,
+		collision_removal = false,
+		attached = player,
+	})
+
 	local soundkey
-	if fire_mod_enabled then
-		soundkey = minetest.sound_play("fire_fire", {
+	if fire_mod_found then
+		soundkey = minetest.sound_play(fire_sound, {
 			to_player = name,
 			loop = true,
 		})
 	end
 
-	qeffects.fire.players[name] = {dps = dps, time = time, hud = hudkey, sound = soundkey}
+	qeffects.fire.players[name] = {dps = dps, time = time, hud = hudkey, sound = soundkey, pkey = particles}
 end
 
 function qeffects.fire.remove(name)
@@ -35,17 +70,58 @@ function qeffects.fire.remove(name)
 
 	if player then
 		player:hud_remove(qeffects.fire.players[name].hud)
+		minetest.delete_particlespawner(qeffects.fire.players[name].pkey)
 	else
 		minetest.log("warning", "[QEffects:Fire] Player "..name.." not found. Finishing remove()...")
 	end
 
-	if fire_mod_enabled then
+	if fire_mod_found then
 		minetest.sound_stop(qeffects.fire.players[name].sound)
 
-		if player then minetest.sound_play("fire_extinguish_flame", {to_player = name}) end
+		if player then minetest.sound_play(fire_extinguish_sound, {to_player = name, pitch = fire_sound_gain}) end
 	end
 
 	qeffects.fire.players[name] = nil
+end
+
+function qeffects.fire.on_step()
+	for _, player in ipairs(minetest.get_connected_players()) do
+		local name = player:get_player_name()
+		local node = minetest.get_node(player:get_pos()).name
+
+		--
+		--- Fire Effect
+		--
+
+		if qeffects.fire.players[name] then
+			if minetest.get_item_group(node, "water") ~= 0 or minetest.get_item_group(node, "snowy") ~= 0 then
+				qeffects.fire.remove(name)
+			end
+		else
+			if minetest.get_item_group(node, "lava") ~= 0 or minetest.get_item_group(node, "igniter") ~= 0 then
+				qeffects.fire.apply(name, 0.7 * qeffects.fire.dps_multiplier, 7)
+			end
+		end
+	end
+
+	if qeffects.fire.players ~= {} then
+		for name, effect in pairs(qeffects.fire.players) do
+			local player = minetest.get_player_by_name(name)
+
+			if player then
+				player:set_hp(player:get_hp() - effect.dps, {quadeffect = "fire"})
+			else
+				qeffects.fire.remove(name)
+				effect = false
+			end
+
+			if effect and effect.time <= 0 then
+				qeffects.fire.remove(name)
+			elseif effect then
+				qeffects.fire.players[name].time = effect.time - 1
+			end
+		end
+	end
 end
 
 minetest.register_on_leaveplayer(function(player)
--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 qeffects = {
 	fire = {
 		players = {},
+		dps_multiplier = 1
 	}
 }
 
@@ -13,67 +14,7 @@ minetest.register_globalstep(function(dtime)
 		step = 0
 	end
 
-	for _, player in ipairs(minetest.get_connected_players()) do
-		local name = player:get_player_name()
-		local node = minetest.get_node(player:get_pos()).name
-
-		--
-		--- Fire Effect
-		--
-
-		if qeffects.fire.players[name] then
-			if minetest.get_item_group(node, "water") ~= 0 or minetest.get_item_group(node, "snowy") ~= 0 then
-				qeffects.fire.remove(name)
-			end
-		else
-			if minetest.get_item_group(node, "lava") ~= 0 or minetest.get_item_group(node, "igniter") ~= 0 then
-				qeffects.fire.apply(name, 1.3, 7)
-			end
-		end
-	end
-
-	--
-	--- Fire Effect
-	--
-
-	if qeffects.fire.players ~= {} then
-		for name, effect in pairs(qeffects.fire.players) do
-			local player = minetest.get_player_by_name(name)
-
-			if player then
-				local pos = player:get_pos()
-
-				player:set_hp(player:get_hp() - effect.dps, {quadeffect = "fire"})
-
-				minetest.add_particlespawner({
-					amount = 20,
-					time = 0.5,
-					minpos = vector.subtract(pos, 0.3),
-					maxpos = vector.add(pos, 0.3),
-					minvel = {x = -1, y = 0, z = -1},
-					maxvel = {x = 1, y = 1,  z = 1},
-					minacc = {x = 0, y = 3, z = 0},
-					maxacc = {x = 0, y = 5, z = 0},
-					minexptime = 0.5,
-					maxexptime = 0.7,
-					minsize = 3,
-					maxsize = 4,
-					texture = "quadeffects_fire_particle.png",
-					collisiondetection = true,
-					collision_removal = true,
-				})
-			else
-				qeffects.fire.remove(name)
-				effect = false
-			end
-
-			if effect and effect.time <= 0 then
-				qeffects.fire.remove(name)
-			elseif effect then
-				qeffects.fire.players[name].time = effect.time - 1
-			end
-		end
-	end
+	qeffects.fire.on_step()
 end)
 
 dofile(minetest.get_modpath("quadeffects").."/fire.lua")
